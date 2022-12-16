26bb1d7219e80a3d1576c5ed48b820027690b2d6
Michelet Saint-Preux <michelet2000@gmail.com>
2022-12-16 12:49:49 +0800

    src/App.js src/components/Card.js src/components/Hero.js src/components/Navbar.js src/style.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import './style.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar />
+      <Hero />
+      <Card />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../style.css'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img 
+                src='https://i.postimg.cc/X7nvY1Xj/airbnb-logo.png' 
+                alt='logo' 
+                className='nav--logo'
+            />
+        </nav>
+    )
+}
