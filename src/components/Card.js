e06dc4088e78a4d0cc873615422cb3d107aaf021
Michelet Saint-Preux <michelet2000@gmail.com>
2022-12-19 01:18:23 +0800

    src/components/Card.js src/style.css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
+
 export default function Card() {
     return (
-        <div>Card</div>
+        <div className='card'>
+            <img 
+                src='https://i.postimg.cc/1zSkYcSt/katie-zaferes.png' 
+                alt='' 
+                className='card--image'
+            />
+            <div className='card--stats'>
+                <img 
+                    src='https://i.postimg.cc/L6R0S47C/star.png' 
+                    alt='' 
+                    className='card--star'
+                />
+                <span>5.0</span>
+                <span className='gray'>(6) . </span>
+                <span className='gray'>USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className='bold'>From $136</span> / Person</p>
+        </div>
     )
 }
